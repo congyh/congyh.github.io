23d15c6e90971688c3cf0ddda8821d4704b87864
congyh <yihao.cong@outlook.com>
2016-12-09 15:40:22 +0800

    Site updated: 2016-12-09 15:40:22

--- hexo-pull.sh
+++ hexo-pull.sh
@@ -3,4 +3,5 @@
 git pull origin hexo
 cp -a _config.yml ../
 cp -a next_config.yml ../themes/next/_config.yml
+cp -a package.json ../package.json
 exit 0
--- hexo-push.sh
+++ hexo-push.sh
@@ -2,6 +2,7 @@
 
 cp -a ../_config.yml ./_config.yml
 cp -a ../themes/next/_config.yml ./next_config.yml
+cp -a ../package.json ./package.json
 git add . -A
 git commit -m "hexo backup"
 git push origin hexo
