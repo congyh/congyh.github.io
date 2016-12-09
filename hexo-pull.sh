6af76c8b756e085d23b4e15df958707c68b93081
congyh <yihao.cong@outlook.com>
2016-12-09 17:40:08 +0800

    hexo backup

--- hexo-pull.sh
+++ hexo-pull.sh
@@ -4,9 +4,9 @@ git pull origin hexo
 cp -a _config.yml ../_config.yml
 cp -a next_config.yml ../themes/next/_config.yml
 cp -a custom.styl ../themes/next/source/css/_variables/custom.styl
-cp -a highlight.styl ../themes/next/source/css/_common/components/highlight/highlight.styl
 cp -a duoshuo-hot-articles.swig ../themes/next/layout/_partials/duoshuo-hot-articles.swig
 cp -a package.json ../package.json
+cp -a highlight.styl ../themes/next/source/css/_common/components/highlight/highlight.styl
 # 不删掉运行hexo g会出现错误
-rm -f ./highlight.styl
+rm -f highlight.styl
 exit 0
--- hexo-push.sh
+++ hexo-push.sh
@@ -8,7 +8,7 @@ cp -a ../themes/next/source/css/_common/components/highlight/highlight.styl ./hi
 cp -a ../themes/next/layout/_partials/duoshuo-hot-articles.swig ./duoshuo-hot-articles.swig
 git add . -A
 git commit -m "hexo backup"
-git push -f origin hexo
+git push origin hexo
 # 不删掉运行hexo g会出现错误
 rm -f ./highlight.styl
 exit 0
